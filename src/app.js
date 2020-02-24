90ccc9619b4fde0672dfe3f137f1b5decbbfda0c
Dotsworthy <dotsworthy@gmail.com>
2020-02-24 21:17:25 +0000

    completed colour coded css for priority

--- src/app.js
+++ src/app.js
@@ -6,22 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
     data: {
       items: [{
         name: "Pay Bill",
-        priority: "High",
-        isComplete: false
-      }]
+        priority: "High"
+      }],
+      newPriority: ""
     },
     methods: {
   saveNewTodo: function() {
     this.items.push({
     name: this.newTodo,
-    isComplete: false,
     priority: this.newPriority
     });
     this.newTodo = "";
   },
-  // buyItem: function(index) {
-  //   this.items[index].isPurchased = !this.items[index].isPurchased;
-  // },
   deleteAll: function() {
     this.items = [];
   }
